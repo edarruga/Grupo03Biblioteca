e4a941af633b6d6dabc96ee4877e53d7e034b550
edarruga <edarruga@CCTL132-05>
2022-11-24 11:59:24 +0100

    edu1

--- ModeloDeDominio/Libro.cs
+++ ModeloDeDominio/Libro.cs
@@ -8,5 +8,36 @@ namespace ModeloDeDominio
 {
     internal class Libro
     {
+        private string isbn;
+        private string titulo;
+        private string autor;
+        private string editorial;
+
+        public Libro(string isbn,string titulo,string autor,string editorial)
+        {
+            this.isbn = isbn;
+            this.titulo = titulo;
+            this.autor = autor;
+            this.editorial = editorial;
+        }
+        public string Isbn
+        {
+            get { return isbn; }
+        }
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+        public string Editorial
+        {
+            get { return editorial; }
+            set { editorial = value; }
+        }
     }
 }
