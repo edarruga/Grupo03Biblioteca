a4b08b301cf171e75b0777db46c750b37995656e
Eduardo Arruga San Martín <edarruga@unirioja.es>
2022-12-01 11:04:16 +0100

    1

---
+++ Persistencia/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    internal class Entity<T> : IEquatable<Entity<T>>
+    {
+        private T id;
+        public Entity(T id) { this.id = id; }
+        public T Id { get { return this.id; } }
+        public bool Equals(Entity<T> other)
+        {
+            if (other==null)
+            {
+                return false;
+            }
+            else
+            {
+                return this.id.Equals(other.id);
+            }
+        }
+    }
+}
